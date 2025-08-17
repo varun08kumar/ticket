d1ecda1a6a0a825aee075dc58933bbdfb533dd37
varun08kumar <91112081+varun08kumar@users.noreply.github.com>
2025-08-17 10:53:24 +0530

    Update index.js

--- ticket/client/pages/index.js
+++ ticket/client/pages/index.js
@@ -1,22 +1,4 @@
-// // import axios from "axios";
-// import buildClient from "../api/build-client";
-// const LandingPage= ({currentUser})=>{
-//     // console.log('i am on client');
-//     // axios.get('/api/users/currentuser')
-//     // {currentUser}
-      
-//     return currentUser?<h1>user signed in </h1>:<h1>chodu signin kar</h1>
-// };
 
-
-// LandingPage.getInitialProps=async (context)=>{
-//     const {req}=context;
-//     console.log(req);
-    
-//     const {data}=await buildClient(context).get('/api/users/currentuser');
-//     return data;
-// }
-// export default LandingPage;
 
 
 import buildClient from "../api/build-client";
